38ac3a273435e5fb12f929ac94d7f198e8c2ce1e
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-05-22 04:04:36 -0300

    Comments removed.

--- test.py
+++ test.py
@@ -3,9 +3,6 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-# create_query = 'CREATE TABLE user (id int, username text, password text)'
-# cursor.execute(create_query)
-
 users = [(1, 'Felipe', 'asdf'), (2, 'rolf', 'qwer'), (3, 'anne', 'zxcv')]
 insert_query = 'insert into user (id, username, password) values (?, ?, ?)'
 cursor.executemany(insert_query, users)
--- user.py
+++ user.py
@@ -1,6 +1,5 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-# from flask import request
 
 class User:
     def __init__(self, _id, username, password):
