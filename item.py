67c09c089e1a4ccd58a96a96c06b9a53c22ac54c
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-06-03 02:51:23 -0300

    Get all items using database.

--- item.py
+++ item.py
@@ -3,10 +3,9 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
-items = [{'name': 'item1', 'price': 2.55}]
+DB_PATH = 'data.db'
 
-def get_item_by_name(name):
-    return next(filter(lambda i: i.get('name') == name, items), None)
+items = [{'name': 'item1', 'price': 2.55}]
 
 class Item:
     def __init__(self, name: str, price: float):
@@ -15,7 +14,7 @@ class Item:
 
     @classmethod
     def get_by_name(cls, name):
-        with sqlite3.connect('data.db') as db_connection:
+        with sqlite3.connect(DB_PATH) as db_connection:
             cursor = db_connection.cursor()
             query = 'SELECT name, price from item where name=?'
             query_result = cursor.execute(query, (name,))
@@ -24,10 +23,28 @@ class Item:
 
     @classmethod
     def insert(cls, item):
-        with sqlite3.connect('data.db') as db_connection:
+        with sqlite3.connect(DB_PATH) as db_connection:
             cursor = db_connection.cursor()
-            cursor.execute('insert into item (name, price) values (?, ?)',
-                           (item['name'], item['price']))
+            query = 'insert into item (name, price) values (?, ?)'
+            cursor.execute(query, (item['name'], item['price']))
+
+    @classmethod
+    def update(cls, item):
+        query = 'UPDATE item SET price=? WHERE name=?'
+        with sqlite3.connect(DB_PATH) as db_connection:
+            cursor = db_connection.cursor()
+            cursor.execute(query, (item['price'], item['name']))
+        return item
+
+    @classmethod
+    def get_all(cls):
+        with sqlite3.connect(DB_PATH) as conn:
+            cursor = conn.cursor()
+            query = 'SELECT name, price from item'
+
+            query_result = cursor.execute(query)
+
+        return [{'name': name, 'price': price} for name, price in query_result]
 
 
 class ItemRegister(Resource):
@@ -49,6 +66,7 @@ class ItemRegister(Resource):
 
         request_data = self.parser.parse_args()
         price = request_data['price']
+
         item = {'name': name, 'price': price}
 
         try:
@@ -59,7 +77,7 @@ class ItemRegister(Resource):
         return {'item': {'name': name, 'price': price}}, 201
 
     def delete(self, name):
-        with sqlite3.connect('data.db') as db_connection:
+        with sqlite3.connect(DB_PATH) as db_connection:
             cursor = db_connection.cursor()
             query = 'DELETE FROM item WHERE name=?'
             cursor.execute(query, (name,))
@@ -71,21 +89,17 @@ class ItemRegister(Resource):
 
     def put(self, name):
         request_data = self.parser.parse_args()
-        price = request_data['price']
+
+        item = {'name': name, 'price': request_data['price']}
+
         if Item.get_by_name(name):
-            update_query = 'UPDATE item SET price=? WHERE name=?'
-            with sqlite3.connect('data.db') as db_connection:
-                cursor = db_connection.cursor()
-                cursor.execute(update_query, (price, name))
-            return {'name': name, 'price': price}
+            Item.update(item)
+            return item
         else:
-            create_query = 'INSERT INTO item (name, price) values (?, ?)'
-            with sqlite3.connect('data.db') as db_connection:
-                cursor = db_connection.cursor()
-                cursor.execute(create_query, (name, price))
-            return {'name': name, 'price': price}, 201
+            Item.insert(item)
+            return item, 201
 
 
 class ItemListRegister(Resource):
     def get(self):
-        return {'items': items}
+        return Item.get_all()
